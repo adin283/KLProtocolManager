441f0aef727879b3e4a2199e5a9a5f4df336b1e3
祝文博 <zhuwenbo@zhujia360.com>
2016-12-29 10:25:07 +0800

    Initial Commit

---
+++ KLProtocolManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KLProtocolManager
+//
+//  Created by Kevin on 2016/12/29.
+//  Copyright © 2016年 KevinLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KLProtocolManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KLProtocolManager
+//
+//  Created by Kevin on 2016/12/29.
+//  Copyright © 2016年 KevinLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
